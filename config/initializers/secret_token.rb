13116afb108a71a01439164a7b12aaa2937e82ae
Richard Davis <rdavis.bacs@gmail.com>
2013-09-05 16:06:20 -0400

    step 3: added staff favorites

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Library::Application.config.secret_key_base = 'e244de67a17f58e618951d3ddeb0860aa63ec9fe4c3dd73dc7d926a4ef377b04ecb7cacde8b4ae25e15a1a8ca5a1b9e61886e00329f72128b94d114238ff861b'
