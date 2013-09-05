bcb81e32fa802851960a2e0dc8696adc86b6f2b4
Richard Davis <rdavis.bacs@gmail.com>
2013-09-05 16:47:12 -0400

    Step 7: Seeded Books table

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ActiveRecord::Base
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :rating, inclusion: 1..100
+
   has_many :checkout
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+columns = [:title, :author, :rating]
+
+books =
+[
+  ["A Brave New World", "Aldous Huxley", 86],
+  ["The Partly Cloudy Patriot", "Sarah Vowell", 75],
+  ["Me Talk Pretty One Day", "David Sedaris", 80],
+  ["The Hitchhiker's Guide to the Galaxy", " Douglas Adams", 90],
+  ["Ender's Game", "Orson Scott Card", 85]
+]
+
+records = []
+books.each do |book|
+  record = {}
+  columns.each_with_index do |col, i|
+    record[col] = book[i]
+  end
+  records << record
+end
+
+records.each do |record|
+  book = Book.where({:title => record[:title],
+                     :author => record[:author]}).first
+
+  if book.nil?
+    book = Book.new(record)
+  else
+    book.assign_attributes(record)
+  end
+  book.save!
+end
